5862ab5d00c205216f40fbf6fe84a9372262bd7c
Kotaro Sugawara <kotarella1110@gmail.com>
2022-08-03 13:48:05 +0900

    build: move babel to swc

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -1,36 +1,56 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { name } = require("./package.json");
 
-module.exports = () => ({
-  entry: "./src/index.tsx",
-  output: {
-    library: `${name}-[name]`,
-    libraryTarget: "umd",
-    chunkLoadingGlobal: `webpackJsonp_${name}`,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".tsx", ".jsx", ".ts"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-  devServer: {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
+module.exports = (_, argv) => {
+  const isProduction = argv.mode == "production";
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: "umd",
+      chunkLoadingGlobal: `webpackJsonp_${name}`,
     },
-    historyApiFallback: true,
-    hot: true,
-    liveReload: false,
-  },
-});
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: "swc-loader",
+            options: {
+              sourceMaps: true,
+              minify: isProduction,
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: "automatic",
+                  },
+                },
+              },
+            },
+          },
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".tsx", ".jsx", ".ts"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+    ],
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      historyApiFallback: true,
+      hot: true,
+      liveReload: false,
+    },
+  };
+};
--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -1,25 +1,45 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = () => ({
-  entry: "./src/index.tsx",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-      },
+module.exports = (_, argv) => {
+  const isProduction = argv.mode == "production";
+  return {
+    entry: "./src/index.tsx",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: "swc-loader",
+            options: {
+              sourceMaps: true,
+              minify: isProduction,
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: "automatic",
+                  },
+                },
+              },
+            },
+          },
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".tsx", ".jsx", ".ts"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".js", ".tsx", ".jsx", ".ts"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-  },
-});
+    devServer: {
+      historyApiFallback: true,
+    },
+  };
+};
