28c4924debf85dde0032c73215bde9a78013ae2e
Kotaro Sugawara <kotarella1110@gmail.com>
2022-08-05 15:35:27 +0900

    feat: use prefetch (#3)

--- shell/src/components/NavBar.tsx
+++ shell/src/components/NavBar.tsx
@@ -1,18 +1,31 @@
+import { prefetchApps } from "qiankun";
+import { useEffect } from "react";
 import { navigateToUrl } from "single-spa";
+import { useHover } from "../hooks/useHover";
 
-export const NavBar = () => (
-  <nav>
-    <ul style={{ display: "flex", listStyle: "none", margin: 0, padding: 0 }}>
-      <li style={{ padding: "8px" }}>
-        <a href="/" onClick={navigateToUrl}>
-          Home
-        </a>
-      </li>
-      <li style={{ padding: "8px" }}>
-        <a href="/react" onClick={navigateToUrl}>
-          React
-        </a>
-      </li>
-    </ul>
-  </nav>
-);
+export const NavBar = () => {
+  const [ref, isHovered] = useHover<HTMLAnchorElement>();
+
+  useEffect(() => {
+    if (isHovered) {
+      prefetchApps([{ name: "reactApp", entry: "//localhost:9001" }]);
+    }
+  }, [isHovered]);
+
+  return (
+    <nav>
+      <ul style={{ display: "flex", listStyle: "none", margin: 0, padding: 0 }}>
+        <li style={{ padding: "8px" }}>
+          <a href="/" onClick={navigateToUrl}>
+            Home
+          </a>
+        </li>
+        <li style={{ padding: "8px" }}>
+          <a ref={ref} href="/react" onClick={navigateToUrl}>
+            React
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
