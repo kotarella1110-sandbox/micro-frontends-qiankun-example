df9f4855d1f389ba4b7aab73dd614a6a96df1c00
neripark <neripark@gmail.com>
2022-08-09 18:13:17 +0900

    global nav to left column and tweak

--- shell/src/App.tsx
+++ shell/src/App.tsx
@@ -1,8 +1,13 @@
 import { NavBar } from "./components/NavBar";
 
 export const App = () => (
-  <>
+  <div style={styleRoot}>
     <NavBar />
     <main id="container" />
-  </>
+  </div>
 );
+
+const styleRoot = {
+  display: "grid",
+  gridTemplateColumns: "264px max-content"
+};
