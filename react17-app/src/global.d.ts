ef99220dcf4b52c2aeefd74e779e7ab0f15089fc
Kotaro Sugawara <kotarella1110@gmail.com>
2022-08-25 10:50:45 +0900

    fix: fix global window type

--- react17-app/src/global.d.ts
+++ react17-app/src/global.d.ts
@@ -1,8 +1,8 @@
 declare global {
   interface Window {
-    __webpack_public_path__: string;
-    __POWERED_BY_QIANKUN__: boolean;
-    __INJECTED_PUBLIC_PATH_BY_QIANKUN__: string;
+    __webpack_public_path__?: string;
+    __POWERED_BY_QIANKUN__?: boolean;
+    __INJECTED_PUBLIC_PATH_BY_QIANKUN__?: string;
   }
 }
 
